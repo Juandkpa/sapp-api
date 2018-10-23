4d6677564949c8da2d45a146d6633b3f4ab7761b
Juan Parra <juandkpa@gmail.com>
2018-10-23 00:21:46 -0500

    group scaffold

---
+++ db/migrate/20181023045604_create_groups.rb
@@ -0,0 +1,11 @@
+class CreateGroups < ActiveRecord::Migration[5.2]
+  def change
+    create_table :groups do |t|
+      t.string :name
+      t.string :fragment
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
